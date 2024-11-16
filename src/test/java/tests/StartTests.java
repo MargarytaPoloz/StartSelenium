c6b64e99e781efadeaca48ea3b0fe78ed5c18b1a
MargarytaPoloz <margarytapoloz@gmail.com>
2024-11-16 14:28:40 +0200

    new commit

--- src/test/java/tests/StartTests.java
+++ src/test/java/tests/StartTests.java
@@ -1,4 +1,18 @@
-package Tests;
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class StartTests {
+    @Test
+    public void firstTEst(){
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://telranedu.web.app/home");
+
+        System.out.println("   hhhhh");
+
+    }
+
+
 }
