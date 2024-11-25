e898b71f19f4eaa94bf83a50d58c5924a57156aa
MargarytaPoloz <margarytapoloz@gmail.com>
2024-11-25 18:44:08 +0200

    ILCarroXPath

--- src/test/java/tests/ILCarroXPath.java
+++ src/test/java/tests/ILCarroXPath.java
@@ -9,20 +9,30 @@ import org.testng.annotations.Test;
 
 public class ILCarroXPath {
     WebDriver driver = new ChromeDriver();
+
     @Test
 
-    public void typeRegForm(){
+    public void typeRegForm() {
         driver.get("https://ilcarro.web.app/registration?url=%2Fsearch");
-WebElement inputName = driver.findElement(By.xpath("//form/div/input[@id='name']"));
+        driver.manage().window().maximize();
+        WebElement inputName = driver.findElement(By.xpath("//input[@id='name']"));
         inputName.sendKeys("Margaryta");
-WebElement inputLastName = driver.findElement(By.xpath("//input[@id='lastName']"));
-inputLastName.sendKeys("Poloz");
-WebElement inputEmail = driver.findElement(By.xpath("//input[@formcontrolname='email']"));
+        WebElement inputLastName = driver.findElement(By.xpath("//input[@id='lastName']"));
+        inputLastName.sendKeys("Poloz");
+        WebElement inputEmail = driver.findElement(By.xpath("//input[@formcontrolname='email']"));
         inputEmail.sendKeys("margarytapoloz@gmail.com");
-WebElement inputPassword = driver.findElement(By.xpath("//input[@formcontrolname='password']"));
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@formcontrolname='password']"));
         inputPassword.sendKeys("123456Margo!");
-       WebElement checkBox = driver.findElement(By.xpath("////input[@type='checkbox']"));
-        checkBox.click();
+        driver.findElement(By.xpath("//label[@for='terms-of-use']")).click();
+
+        //WebElement checkBox = driver.findElement(By.xpath("//input[@type='checkbox']"));
+        //checkBox.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         WebElement btnSubmit = driver.findElement(By.xpath("//button[@type='submit']"));
         btnSubmit.click();
     }
